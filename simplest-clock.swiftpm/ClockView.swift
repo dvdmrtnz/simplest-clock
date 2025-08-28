885f365337f9897162ac540e75ea7defb8bb4edc
David Martinez <47610359+dvdmrtnz@users.noreply.github.com>
2025-08-28 16:57:13 +0200

    Add initial files

---
+++ simplest-clock.swiftpm/ClockView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct ClockView: View {
+    @State private var now = Date()
+    // 24h formatter
+    private static let df: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "en_GB") // ensures 24h symbols
+        f.dateFormat = "HH mm ss"
+        return f
+    }()
+    
+    var body: some View {
+        Text(Self.df.string(from: now))
+            .font(.system(size: 1000, weight: .bold))
+            .minimumScaleFactor(0.1)
+            .monospacedDigit()
+            .lineLimit(1)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
+            .foregroundStyle(.white)
+            .scaleEffect(x: 1, y: 2)
+            .onReceive(Timer.publish(every: 1, on: .main, in: .common).autoconnect()) { _ in
+                now = Date()
+            }
+    }
+}
---
+++ simplest-clock.swiftpm/SimplestClockApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SimplestClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ClockView()
+        }
+    }
+}
